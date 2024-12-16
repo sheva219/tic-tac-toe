eb64e612c04fdf25b67d3797ebace1e6fb53a27c
sheva219 <viacheslav.shevchenko000@gmail.com>
2024-12-16 14:10:25 -0300

    Update gane logic

--- src/App.js
+++ src/App.js
@@ -8,16 +8,54 @@ function Square({ value, onSquareClick }) {
   );
 }
 
+function calculateWinner(squares) {
+  const winnerLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8]
+  ];
+  for (let i = 0; i < winnerLines.length; i++) {
+    const [a, b, c] = winnerLines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+}
+
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
 
   function handleClick(index) {
+    if (calculateWinner(squares) || squares[index]) {
+      return;
+    }
+
     const nextSquare = squares.slice();
-    nextSquare[index] = 'X';
+    nextSquare[index] = xIsNext === true ? 'X' : 'O';
     setSquares(nextSquare);
+    setXIsNext(!xIsNext);
   }
+
+  const winner = calculateWinner(squares);
+
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
